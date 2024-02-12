dbca986affac992b3aee26f8bb73f1ebb627cb77
Pauline Washukenyi <paulinewashukenyi@gmail.com>
2024-02-12 13:33:27 +0100

    update Portfolio

--- src/components/sections/projects/ProjectsSection.tsx
+++ src/components/sections/projects/ProjectsSection.tsx
@@ -13,29 +13,44 @@ export interface ProjectsSectionProps {
 export default function ProjectsSection({ onClick }: ProjectsSectionProps) {
   const { open, handleOpen } = useOpen();
   const [project, setProject] = useState({} as Project);
+  const [hovered, setHovered] = useState(false);
+
+  const handleMouseEnter = () => setHovered(true);
+  const handleMouseLeave = () => setHovered(false);
 
   return (
     <>
-    <section className="section3 h-[90vh] p-4 flex flex-col gap-4 justify-center scroll-mt-[10vh] snap-start overflow-auto">
-      <SectionTitle title="Portfolio" color="bg-pink-700" className="place-self-center" />
-      <div className="flex flex-col md:items-center md:justify-center md:flex-wrap gap-x-0 gap-4 h-full slide">
+      <section className="section3 h-[90vh] p-4 pt-6 flex flex-col gap-6 justify-center scroll-mt-[10vh] snap-start overflow-auto">
+        <SectionTitle title="Portfolio" color="bg-pink-700" className="place-self-center" />
+        <div className="flex flex-col md:flex-row gap-4 h-full overflow-hidden slide">
 
-        {projects.map((project) => (
-          <article
-          key={project.id}
-          className={`bg-cover bg-center flex-1 md:flex-none md:h-[47.5%] md:w-[47.5%] rounded-lg cursor-pointer relative`}
-          style={{ backgroundImage: `url(${project.backgroundImage})` }}
-          onClick={() => { onClick(); handleOpen(); setProject(project) }}
-        >
-          <p className="font-Bangers py-2 px-4 absolute bottom-2 left-2 text-3xl bg-slate-100 rounded-sm border-black border-2">
-            {project.title}
-            </p>
-        </article>
-        ))}
+          {projects.map((project) => (
+            <article
+              key={project.id}
+              className={`bg-cover bg-center flex-1 rounded-lg cursor-pointer relative hover:flex-[5] transition-all ease-in-out flex overflow-hidden ${hovered ? "items-center justify-center" : ""}`}
+              style={{ backgroundImage: `url(${project.backgroundImage})` }}
+              onClick={() => { onClick(); handleOpen(); setProject(project) }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
 
-      </div>
-    </section>
-    {open && <ProjectModal onClick={() => { onClick(); handleOpen(); }} project={project} />}
+            >
+              <p 
+              className={`font-Bangers py-2 px-4 absolute text-3xl bg-slate-100 rounded-sm border-black border-2 ${hovered ? "z-50 text-9xl" : " bottom-2 left-2"}`}
+              >
+                {project.title}
+              </p>
+              {hovered &&(
+                <span className="h-full w-full bg-white-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 absolute">
+
+                </span>
+              )
+              }
+            </article>
+          ))}
+
+        </div>
+      </section>
+      {open && <ProjectModal onClick={() => { onClick(); handleOpen(); }} project={project} />}
     </>
   );
 }
--- src/components/utils/utils.tsx
+++ src/components/utils/utils.tsx
@@ -1,6 +1,7 @@
 // utils.js
 import { useState } from "react";
 
+// Ouverture/fermeture modal & menu
 export const useOpen = () => {
   const [open, setOpen] = useState(false);
 
@@ -9,5 +10,12 @@ export const useOpen = () => {
   };
 
   return { open, handleOpen };
+};
+
+// Animation mouvement 3D (hover)
+export const handleMouseMove = (e: React.MouseEvent<HTMLImageElement>) => {
+  const xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+  const yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+  e.currentTarget.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 };
 
